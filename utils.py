004d4a47e2ccf8c97e8bfea498138342e30c7009
kolomiets <ak.519@yandex.ru>
2019-03-13 13:01:11 +0300

    Функция для метрик

--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 import json
+from typing import List
+from rouge import Rouge
 
 
 def load_data(path: str):
     with open(path, 'r') as f:
         data = json.load(f)
     return data
+
+
+def get_metrics(true_titles: List[str], predicted_titles: List[str]) -> dict:
+    scorer = Rouge(apply_avg=True)
+    scores = scorer.get_scores(predicted_titles, true_titles)
+    return scores
