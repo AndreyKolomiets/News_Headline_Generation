375d2559f13f7c5a03883de16a47ba03ea21d8b8
kolomiets <ak.519@yandex.ru>
2019-05-07 19:35:43 +0300

    Сделал небольшое число итераций, для профилирования

--- pointer_summarizer/data_util/config.py
+++ pointer_summarizer/data_util/config.py
@@ -26,11 +26,11 @@ trunc_norm_init_std = 1e-4
 max_grad_norm = 2.0
 
 pointer_gen = True
-is_coverage = True
+is_coverage = False
 cov_loss_wt = 0.2
 
 eps = 1e-12
-max_iterations = 500000
+max_iterations = 500
 
 use_gpu = True
 
